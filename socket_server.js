8358677cc1b98bf8b573dbe5a7719e9d83fc9dca
Todor Lubenov <todorlubenov@Todors-MacBook-Pro-2.local>
2018-07-24 07:59:53 +0300

    add socket_server.js

---
+++ socket_server.js
@@ -0,0 +1,37 @@
+// Load TCP Lib
+net = require('net');
+
+// Keep track on GPRS Clients
+var gprs_device_list = [];
+
+// Start TCP Server
+net.createServer((socket) => {
+
+    // Identify this device
+    socket.name = socket.remoteAddress + ":" + socket.repotePort;
+
+    //  Put new device to the list
+    gprs_device_list.push(socket);
+
+    //  Log device to the database
+    //  Log to Mongo/Rethink DB logic here
+
+    socket.on('data', (data) => {
+        handleGPRS(socket.name + "> " + data, socket);
+    });
+
+    socket.on('end', () => {
+        handleGPRS(socket.name + " leave the system");
+    });
+
+    // handleGPRS function processing GPRS messages
+    function handleGPRS(message, sender) {
+        // save message to database and handle it
+
+        process.stdout.write(message);
+    }
+
+}).listen(3500);
+
+//Push message on server terminal
+console.log("TCP Server running on port 3500 ...");
